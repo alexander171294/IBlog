c5aa0ce80a618dc1556dd782e08f507e05ad96f2
Alexander Eberle <axelander_xd@live.com.ar>
2012-08-05 17:58:35 -0300

    la db actualizada.

--- iblog_db.sql
+++ iblog_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 03-08-2012 a las 05:05:49
+-- Tiempo de generación: 05-08-2012 a las 22:57:55
 -- Versión del servidor: 5.5.8
 -- Versión de PHP: 5.3.5
 
@@ -28,6 +28,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `categorias` (
   `cat_id` int(10) NOT NULL AUTO_INCREMENT,
   `cat_nombre` varchar(200) NOT NULL,
+  `cat_seo` varchar(250) NOT NULL,
   PRIMARY KEY (`cat_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
@@ -35,8 +36,28 @@ CREATE TABLE IF NOT EXISTS `categorias` (
 -- Volcar la base de datos para la tabla `categorias`
 --
 
-INSERT INTO `categorias` (`cat_id`, `cat_nombre`) VALUES
-(1, 'categoria de prueba');
+INSERT INTO `categorias` (`cat_id`, `cat_nombre`, `cat_seo`) VALUES
+(1, 'categoria de prueba', 'categoria-de-prueba');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `censura`
+--
+
+CREATE TABLE IF NOT EXISTS `censura` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bad` varchar(250) NOT NULL,
+  `good` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcar la base de datos para la tabla `censura`
+--
+
+INSERT INTO `censura` (`id`, `bad`, `good`) VALUES
+(1, 'puto', '[CENSURADO]');
 
 -- --------------------------------------------------------
 
@@ -53,15 +74,19 @@ CREATE TABLE IF NOT EXISTS `comentarios` (
   `pub` int(10) NOT NULL,
   `fecha` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Volcar la base de datos para la tabla `comentarios`
 --
 
 INSERT INTO `comentarios` (`id`, `Name`, `email`, `web`, `coment`, `pub`, `fecha`) VALUES
-(1, 'alex', 'alexander171294@live.com', 'http://alebcorp.com.ar', 'asdasdasdasd', 1, 1343930635),
-(2, 'Alexander', 'alexander171294@live.com', '/', 'sadasdasdsad', 1, 1343932967);
+(7, 'Alexander', 'alexandereberle94@hotmail.com', '/', 'sadasdasd', 1, 1344149545),
+(8, 'Alexander', 'alexandereberle94@hotmail.com', '/', '<b>Hola cabrÃ³n</b>', 1, 1344149576),
+(9, 'Alexander', 'alexandereberle94@hotmail.com', '/', '', 1, 1344150373),
+(10, 'Alexander', 'alexandereberle94@hotmail.com', '/', 'asd <a href="http://google.com">fsd</a><br />\r\nsdf<br />\r\n<br />\r\n', 1, 1344150484),
+(11, 'asdasd', 'alexander171294@live.com', '/', 'asdasdsad prueba <b>Hola</b>', 1, 1344187757),
+(12, 'prueba con seo', 'asereje a deje dejede tu dejede seiu noua', '/', 'feos todos feos', 1, 1344199176);
 
 -- --------------------------------------------------------
 
@@ -75,18 +100,42 @@ CREATE TABLE IF NOT EXISTS `menu` (
   `link` text NOT NULL,
   `menu` int(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Volcar la base de datos para la tabla `menu`
 --
 
 INSERT INTO `menu` (`id`, `nombre`, `link`, `menu`) VALUES
-(1, 'prueba', 'http://google.com', 1),
-(2, 'prueba principal', 'sadasd', 2),
-(3, 'lololo', 'sadasd', 2),
-(5, 'afiliado', 'sadasd', 3),
-(6, 'hlasd', 'asdasd', 4);
+(1, 'prueba', 'http://localhost/pagina/1/pagina-de-prueba/', 1),
+(2, 'Inicio', 'http://localhost/index.php', 2),
+(3, 'I-Blog', 'https://github.com/alexander171294/IBlog', 3),
+(4, 'Acceder', 'http://localhost/acceder/', 4),
+(5, 'Registrarse', 'http://localhost/registro/', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paginas`
+--
+
+CREATE TABLE IF NOT EXISTS `paginas` (
+  `pag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `seo_title` varchar(250) NOT NULL,
+  `pag_keys` varchar(250) NOT NULL,
+  `pag_nombre` varchar(250) NOT NULL,
+  `pag_contenido` text NOT NULL,
+  `pag_autor` int(11) NOT NULL,
+  `pag_fecha` int(11) NOT NULL,
+  PRIMARY KEY (`pag_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcar la base de datos para la tabla `paginas`
+--
+
+INSERT INTO `paginas` (`pag_id`, `seo_title`, `pag_keys`, `pag_nombre`, `pag_contenido`, `pag_autor`, `pag_fecha`) VALUES
+(1, 'pagina-de-prueba', 'asdasd', 'P&aacute;gina de Prueba', 'lallalalal<br>\r\nsradasd<br>\r\nesto es una prueba jjeje', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -104,6 +153,7 @@ CREATE TABLE IF NOT EXISTS `publicaciones` (
   `pub_comentario` int(10) NOT NULL,
   `pub_categoria` int(10) NOT NULL,
   `pub_fecha` int(10) NOT NULL,
+  `seo_title` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`pub_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
@@ -111,8 +161,8 @@ CREATE TABLE IF NOT EXISTS `publicaciones` (
 -- Volcar la base de datos para la tabla `publicaciones`
 --
 
-INSERT INTO `publicaciones` (`pub_id`, `pub_keys`, `pub_nombre`, `pub_preview`, `pub_contenido`, `pub_autor`, `pub_comentario`, `pub_categoria`, `pub_fecha`) VALUES
-(1, '', 'Prueba', 'prueba preview', 'prueba completa', 1, 0, 1, 1343889618);
+INSERT INTO `publicaciones` (`pub_id`, `pub_keys`, `pub_nombre`, `pub_preview`, `pub_contenido`, `pub_autor`, `pub_comentario`, `pub_categoria`, `pub_fecha`, `seo_title`) VALUES
+(1, '', 'Prueba', 'prueba preview', 'prueba completa', 1, 7, 1, 1343889618, 'prueba');
 
 -- --------------------------------------------------------
 
@@ -157,11 +207,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `u_pass` varchar(200) NOT NULL,
   `u_rango` int(1) NOT NULL,
   PRIMARY KEY (`u_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Volcar la base de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`u_id`, `u_nombre`, `u_pass`, `u_rango`) VALUES
-(1, 'alex', '', 0);
+(1, 'alexander1712', '$2a$08$RnFaOFSMdIa/mjMtkdCQU.Cy8sa7GI9o0khb9E2iXZrpzRWGQrtRy', 0);
