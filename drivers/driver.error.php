3016c0abefe4af49deb5d5bccaf4e9dbe7676935
Alexander Eberle <axelander_xd@live.com.ar>
2012-08-14 09:33:45 -0300

    agregué el menu al sistema de error

---
+++ drivers/driver.error.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * I-Blog
+ *
+ * PHP Version 5.3
+ *
+ * I-Blog es un proyecto Open Source desarrollado para ser rápido, y sencillo.
+ *
+ * El repositorio oficial del proyecto lo puedes encontrar un poco más abajo,
+ * iblog fué diseñado para blogs alojados en servidores pequeños
+ * que no cuenten con tantos recursos para tener un blog pesado como son otros
+ * sistemas de blog. .
+ *
+ * @author    Alexander Eberle Renzulli <alexander171294@live.com>
+ * @copyright 2012 AlEb Corporation / (http://www.alebcorp.com.ar)
+ * @license   http://www.gnu.org/copyleft/gpl.html
+ * @link      https://github.com/alexander171294/IBlog
+ */
+
+/**
+ * driver.error.php
+ *
+ * Éste controlador se encarga de gestionar los errores
+ *
+ * @author  Alexander1712 <alexander171294@live.com>
+ * @license http://www.gnu.org/copyleft/gpl.html
+ * @link    https://github.com/alexander171294/IBlog
+ */
+
+/* ////////////////////////////////////////////////////////
+               Configuración del menú
+*/
+// crear una instancia de la clase (recordar que tenemos autocarga de clases activado)
+$menu = new Menu($db);
+// asignamos el menu lateral
+$rain->assign('menu_lateral',$menu->get_menu(1));
+// asignamos el menu principal
+$rain->assign('menu_principal',$menu->get_menu(2));
+// asignamos el menu de afiliados
+$rain->assign('menu_afiliados',$menu->get_menu(3));
+// asignamos el menu inferior
+$rain->assign('menu_inferior',$menu->get_menu(4));
+// borramos la variable que contiene la clase menu
+unset($menu);
+//////////////////////////////////////////////////////////
